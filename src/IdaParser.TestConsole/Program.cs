fcddc2a6575acfc4ab58eb9bdf6a94144ddeee70
Bia10 <t.locke1234@gmail.com>
2021-07-26 15:03:27 +0200

    continue

--- src/IdaParser.TestConsole/Program.cs
+++ src/IdaParser.TestConsole/Program.cs
@@ -4,17 +4,18 @@ namespace IdaParser.TestConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             const string path = @"C:\Maplestory95.exe.h";
-
             var parser = new Parser();
-            var results = parser.SearchForStructureInHeaderFile(path, "struct CItemInfo::INCLEVELITEM");
 
-            foreach(var rslt in results)
-            {
-                Console.WriteLine("Idx: {0} Word: {1}", rslt.Index, rslt.Word);
-            }
+            //var results = parser.SearchForStructInHeaderFile(path, "struct CItemInfo::INCLEVELITEM");
+            //foreach(var rslt in results)
+                //Console.WriteLine("Idx: {0} Word: {1}", rslt.Index, rslt.Word);
+
+            var results2 = parser.SearchForStructInHeaderFile2(path, "struct CItemInfo::INCLEVELITEM");
+            foreach (var rslt in results2)
+                Console.WriteLine("Idx: {0} Word: {1}", rslt.Item1, rslt.Item2);
         }
     }
 }
--- src/IdaParser/Parser.cs
+++ src/IdaParser/Parser.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Utils.Enumeration;
 using Utils.Types.String;
 
 namespace IdaParser
@@ -13,7 +14,7 @@ namespace IdaParser
         {
         }
 
-        public IEnumerable<WordMatch> SearchForStructureInHeaderFile(string pathToHeaderFile, string structureName)
+        public IEnumerable<WordMatch> SearchForStructInHeaderFile(string pathToHeaderFile, string structName)
         {
             if (!pathToHeaderFile.Valid())
                 return null;
@@ -22,15 +23,32 @@ namespace IdaParser
                 File.ReadLines(pathToHeaderFile).Where(searchLine 
                     => !string.IsNullOrEmpty(searchLine)).ToList();
 
-            Console.WriteLine(lines.Count);
+            Console.WriteLine("Non-Empty Lines: {0}", lines.Count);
 
             var searchText = new AhoCorasick(lines);
-            var result = searchText.Search(structureName).ToList();
+            var result = searchText.Search(structName).ToList();
 
             if (result.Count == 0)
                 Console.WriteLine("NOT FOUND!");
 
             return result;
+        }
+
+        public IEnumerable<Tuple<int, string>> SearchForStructInHeaderFile2(string pathToHeaderFile, string structName)
+        {
+            if (!pathToHeaderFile.Valid())
+                return null;
+
+            var results = new List<Tuple<int, string>>();
+            var lines = File.ReadLines(pathToHeaderFile).ToList();
+
+            Console.WriteLine("Lines: {0}", lines.Count);
+
+            foreach (var (line, idx) in lines.WithIndex())
+                if (line.Contains(structName))
+                    results.Add(new Tuple<int, string>(idx + 1, line));
+
+            return results;
         }
     }
 }
