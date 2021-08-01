816aeb4511e00bed3f12931f95c6afe38bb75f2e
Bia10 <t.locke1234@gmail.com>
2021-08-01 23:57:47 +0200

    refactor

--- src/IdaParser.CppParser/DefinitionFile.cs
+++ src/IdaParser.CppParser/DefinitionFile.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using Utils.Enumeration;
 using Utils.Types.Enum;
+using Utils.Types.String;
 
 namespace IdaParser.CppParser
 {
@@ -100,114 +101,24 @@ namespace IdaParser.CppParser
             }
         }
 
-        public bool AreStringsEqual(string strA, string strB)
-        {
-            var result = string.CompareOrdinal(strA, strB);
-
-            return result == 0;
-        }
-
-        public bool AreCharEqual(string strA, string strB)
-        {
-            var aSet = new HashSet<char>(strA);
-            var bSet = new HashSet<char>(strB);
-
-            var areHashEqual = aSet.SetEquals(bSet);
-            var areStringEqual = AreStringsEqual(strA, strB);
-
-            Console.WriteLine("strA {0} strB: {1} HashEqual: {2} StringEqual: {3}", strA, strB, areHashEqual, areStringEqual);
-
-            return areHashEqual && areStringEqual;
-        }
-
-        public string GetSubStringSizeOfTypeStr(string str, string typeStr)
-        {
-            return str.Length >= typeStr.Length ? str[..typeStr.Length] : string.Empty;
-        }
-
         public string GetBasicType(string line)
         {
-            return line switch
+            foreach (SizedIntegers enumEntry in Enum.GetValues(typeof(SizedIntegers)))
             {
-                _ when line.Contains(SizedIntegers.Char.GetDescription()) 
-                    => SizedIntegers.Char.GetDescription(),
-                _ when line.Contains(SizedIntegers.Short.GetDescription())
-                    => SizedIntegers.Short.GetDescription(),
-                _ when line.Contains(SizedIntegers.Int.GetDescription())
-                    => SizedIntegers.Int.GetDescription(),
-                _ when line.Contains(SizedIntegers.LongLong.GetDescription())
-                    => SizedIntegers.LongLong.GetDescription(),
+                var enumDescription = enumEntry.GetDescription();
 
-                _ when AreCharEqual(CppDataTypes.UnsignedShortInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.UnsignedShortInt.GetDescription()))
-                    => CppDataTypes.UnsignedShortInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.UnsignedLongInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.UnsignedLongInt.GetDescription()))
-                    => CppDataTypes.UnsignedLongInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.SignedShortInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.SignedShortInt.GetDescription()))
-                    => CppDataTypes.SignedShortInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.SignedLongInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.SignedLongInt.GetDescription()))
-                    => CppDataTypes.SignedLongInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.UnsignedShort.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.UnsignedShort.GetDescription()))
-                    => CppDataTypes.UnsignedShort.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.UnsignedLong.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.UnsignedLong.GetDescription()))
-                    => CppDataTypes.UnsignedLong.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.UnsignedInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.UnsignedInt.GetDescription()))
-                    => CppDataTypes.UnsignedInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.SignedShort.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.SignedShort.GetDescription()))
-                    => CppDataTypes.SignedShort.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.SignedLong.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.SignedLong.GetDescription()))
-                    => CppDataTypes.SignedLong.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.SignedLong.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.SignedLong.GetDescription()))
-                    => CppDataTypes.SignedLong.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.SignedInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.SignedInt.GetDescription()))
-                    => CppDataTypes.SignedInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.ShortInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.ShortInt.GetDescription()))
-                    => CppDataTypes.ShortInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.Unsigned.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.Unsigned.GetDescription()))
-                    => CppDataTypes.Unsigned.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.LongInt.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.LongInt.GetDescription()))
-                    => CppDataTypes.LongInt.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.Signed.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.Signed.GetDescription()))
-                    => CppDataTypes.Signed.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.Short.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.Short.GetDescription()))
-                    => CppDataTypes.Short.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.Long.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.Long.GetDescription()))
-                    => CppDataTypes.Long.GetDescription(),
-                _ when AreCharEqual(CppDataTypes.Int.GetDescription(),
-                        GetSubStringSizeOfTypeStr(line, CppDataTypes.Int.GetDescription()))
-                    => CppDataTypes.Int.GetDescription(),
+                if (enumDescription.StrictlyCharEqualTo(line.HeadSizeOfStrB(enumDescription)))
+                    return enumDescription;
+            }
+            foreach (CppDataTypes enumEntry in Enum.GetValues(typeof(CppDataTypes)))
+            {
+                var enumDescription = enumEntry.GetDescription();
 
-                /*_ when line.Contains(CppDataTypes.LongLong.GetDescription())
-                    => CppDataTypes.LongLong.GetDescription(),
-                _ when line.Contains(CppDataTypes.LongLongInt.GetDescription())
-                    => CppDataTypes.LongLongInt.GetDescription(),
-                _ when line.Contains(CppDataTypes.SignedLongLong.GetDescription())
-                    => CppDataTypes.SignedLongLong.GetDescription(),
-                _ when line.Contains(CppDataTypes.SignedLongLongInt.GetDescription())
-                    => CppDataTypes.SignedLongLongInt.GetDescription(),
-                _ when line.Contains(CppDataTypes.UnsignedLongLong.GetDescription())
-                    => CppDataTypes.UnsignedLongLong.GetDescription(),
-                _ when line.Contains(CppDataTypes.UnsignedLongLongInt.GetDescription())
-                    => CppDataTypes.UnsignedLongLongInt.GetDescription(), */
+                if (enumDescription.StrictlyCharEqualTo(line.HeadSizeOfStrB(enumDescription)))
+                    return enumDescription;
+            }
 
-                _ => string.Empty
-            };
+            return string.Empty;
         }
     }
 }
